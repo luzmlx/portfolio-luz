7959675294202e2a47b51aa62a67dfee609db4d2
luzmlx <luzlopez0820@gmail.com>
2021-12-04 01:26:06 -0500

    finished checking email requirement:

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 let themeDots = document.getElementsByClassName("theme-dot")
 
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+}else {
+    setTheme(theme)
+}
+
 for (var i=0; themeDots.length > i; i++) {
     themeDots[i].addEventListener('click', function(){
-        
+        let mode = this.dataset.mode
+        setTheme(mode)
     })
+}
+
+function setTheme(mode){
+    if(mode == 'light') {
+        document.getElementById('theme-style').href = "style.css"
+    }
+
+    if(mode == 'dark') {
+        document.getElementById('theme-style').href = "blue.css"
+    }
+
+    localStorage.setItem('theme', mode)
+
 }
